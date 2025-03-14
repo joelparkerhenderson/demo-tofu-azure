3d833aabc38915c5eca6e61bffef6917ecc9a4c1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-14 14:24:22 +0000

    Add outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "azurerm_resource_group" {
+  value = azurerm_resource_group.demo.name
+}
+
+output "azurerm_public_ip.demo.ip_address" {
+  value = azurerm_public_ip.demo.ip_address
+}
+
+output "azurerm_postgresql_server.demo.fqdn" {
+  value = azurerm_postgresql_server.demo.fqdn
+}
+
+output "azurerm_postgresql_database.demo.name" {
+  value = azurerm_postgresql_database.demo.name
 }
