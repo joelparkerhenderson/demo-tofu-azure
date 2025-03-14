b9bfb90497be6bba38129af64e8b71c624b5f218
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-14 14:25:22 +0000

    Fix output names

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "azurerm_resource_group" {
+output "azurerm_resource_group--demo--name" {
   value = azurerm_resource_group.demo.name
 }
 
-output "azurerm_public_ip.demo.ip_address" {
+output "azurerm_public_ip--demo--ip_address" {
   value = azurerm_public_ip.demo.ip_address
 }
 
-output "azurerm_postgresql_server.demo.fqdn" {
+output "azurerm_postgresql_server--demo--fqdn" {
   value = azurerm_postgresql_server.demo.fqdn
 }
 
-output "azurerm_postgresql_database.demo.name" {
+output "azurerm_postgresql_database--demo--name" {
   value = azurerm_postgresql_database.demo.name
 }
