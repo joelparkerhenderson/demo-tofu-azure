78907c8cbfe8fc67c0a1a4095c1ed18ceda5243e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-03-14 19:29:59 +0000

    Fix providers features

--- providers.tf
+++ providers.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+    features {}
+}
+
 terraform {
   required_providers {
     azurerm = {
@@ -8,7 +12,20 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
   }
   # Optional: Set the backend to store the state file
-  backend "azurerm" {}
+  # backend "azurerm" {
+  #   resource_group_name  = "my-resource-group"
+  #   storage_account_name = "mystorageaccount"
+  #   container_name       = "my-container"
+  #   key                  = "tofu.tfstate"
+  #   subscription_id      = ""
+  #   client_id            = ""
+  #   tenant_id            = ""
+  #   client_secret        = ""
+  # }
 }
